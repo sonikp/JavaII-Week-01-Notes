f1604a2b48b22962d7f49f6d5f1b9cbe07ea3d25
Michael.Floerchinger <michael.floerchinger@am.sony.com>
2016-09-26 20:21:05 -0700

    Finished @ work

--- src/Turtle.java
+++ src/Turtle.java
@@ -86,34 +86,34 @@ public class Turtle extends SimpleTurtle
 	  this.forward(30);
   }
   
-  public void drawSquare2()
-  {
-	  
-	  int length = 100;
-	  this.turnRight();
-	  this.forward(length);
-	  this.turnRight();
-	  this.forward(length);
-	  this.turnRight();
-	  this.forward(length);
-	  this.turnRight();
-	  this.forward(length);
-  }
-  
-  public void drawSquareParam(int length)
+  public void drawParameterizedRectangle(int width, int height)
   {
 	  
 	  this.turnRight();
-	  this.forward(length);
+	  this.forward(width);
 	  this.turnRight();
-	  this.forward(length);
+	  this.forward(height);
 	  this.turnRight();
-	  this.forward(length);
+	  this.forward(width);
 	  this.turnRight();
-	  this.forward(length);
+	  this.forward(height);
   }
   
-  public void drawRectangleParam(int length)
+  public void drawSquareRightParam(int length)
+  {
+	  
+	  this.setPenColor(Color.magenta);
+	  this.forward(length);
+	  this.turnRight();
+	  this.forward(length);
+	  this.turnRight();
+	  this.forward(length);
+	  this.turnRight();
+	  this.forward(length);
+	  this.turnRight();
+  }
+  
+  public void drawRectangleLeftParam(int length)
   {
 	  
 	  this.turnLeft();
@@ -126,54 +126,145 @@ public class Turtle extends SimpleTurtle
 	  this.forward(length * 2);
   }
   
+ public void drawHexagonRight(int length)
+ {
+	 
+	 this.setPenColor(Color.blue);
+	 this.forward(length);
+	 this.turn(60);
+	 this.forward(length);
+	 this.turn(60);
+	 this.forward(length);
+	 this.turn(60);
+	 this.forward(length);
+	 this.turn(60);	 
+	 this.forward(length);
+	 this.turn(60);
+	 this.forward(length);
+	 this.turn(60);
+	 
+	 
+ }
+ 
+ public void drawPentagonRight(int length)
+ {
+	 
+	 this.setPenColor(Color.orange);
+	 this.forward(length);
+	 this.turn(72);
+	 this.forward(length);
+	 this.turn(72);
+	 this.forward(length);
+	 this.turn(72);
+	 this.forward(length);
+	 this.turn(72);
+	 this.forward(length);
+	 this.turn(72);
+	 
+	 
+ }
+ 
+ public void drawCrazySpiral()
+ {
+	  for (int j = 1; j <= 230; j+=1) 
+	  {
+		  this.setPenColor(Color.green);
+		  this.moveTo(320,240);		  
+		  this.turn(5); 
+		  this.forward(j);
+	  }
+	  
+	  this.turnRight();
+ }
+  
+ public void drawCircle()
+ {
+	 // relocating position
+	 this.penUp();
+	 this.forward(200);
+	 this.setPenColor(Color.red);
+	 this.penDown();
+	 this.turnRight();
+	  	  
+	  // draw circle
+	  for (int i = 1; i <= 360; i+=10 )
+	  {
+		  this.forward(38);
+		  this.turn(10);
+		  
+	  }
+ }
+ 
+ 
+ 
   public static void main(String[] args)
   {
     
-	// Assignment - need methods for drawRectangle, drawHexagon, drawPentagon
+	  // Assignment - need methods for drawRectangle, drawHexagon, drawPentagon, something interesting
+	  // The mating rituals of the Javian Mathematical Turtle
 	
-	  World earth = new World();
-	  Turtle bertyDaTurtle = new Turtle(earth);
-	  Turtle berthaDaOtherTurtle = new Turtle(earth);
+	  // Create world objects and turtle objects
+	  World duckPond = new World();
+	  Turtle bertyDaTurtle = new Turtle(duckPond);
+	  Turtle berthaDaOtherTurtle = new Turtle(duckPond);
 	  
-	  bertyDaTurtle.drawSquareParam(60);
-	  bertyDaTurtle.drawRectangleParam(60);
+	  // Setup turtle information
+	  duckPond.setName("DuckPond");
+	  bertyDaTurtle.setName("Berty");
+	  berthaDaOtherTurtle.setName("Bertha");
+	  
+	  // Berty, marking territorial boundaries
+	  bertyDaTurtle.drawCircle();
+	  
+	  // Mating ritual of Bertha; strutting her stuff
 	  berthaDaOtherTurtle.penUp();
-	  berthaDaOtherTurtle.moveTo(40,90);
+	  berthaDaOtherTurtle.moveTo(250,110);
+	  berthaDaOtherTurtle.penDown();
+	  berthaDaOtherTurtle.turn(90);
+	  berthaDaOtherTurtle.drawHexagonRight(150);
+	  berthaDaOtherTurtle.drawPentagonRight(150);
+	  berthaDaOtherTurtle.drawSquareRightParam(150);
+
 	  
+	  // Berty, going a little crazy in the pond
+	  bertyDaTurtle.drawCrazySpiral();
+	  bertyDaTurtle.setPenColor(Color.black);
+	  bertyDaTurtle.turn(-70);
+	  bertyDaTurtle.drawRectangleLeftParam(173);
+	  bertyDaTurtle.turn(156);
+	  bertyDaTurtle.forward(440);
+	  bertyDaTurtle.turn(-66);
+	  bertyDaTurtle.setPenColor(Color.blue);
+	  bertyDaTurtle.forward(110);
+	  bertyDaTurtle.drawSquare();
+	  bertyDaTurtle.turn(180);
+	  bertyDaTurtle.forward(90);
 	  
-	  /*
-		// first pass assignment
-		World earth = new World();
-		Turtle turtleSqrParm = new Turtle(earth);
-		turtleSqrParm.drawSquareParam(30);
-		
-		Turtle turtleRecParm = new Turtle(earth);
-		turtleRecParm.drawRectangleParam(90);
-		*/
+	  // Bertha realizes Berty is a complete loon, and she's into that sort of thing!!!
+	  berthaDaOtherTurtle.turn(241);
+	  berthaDaOtherTurtle.forward(40);
 	  
+	  // ahhhh, g'day mate, what are you doing here?
+	  // (DebugCode: find out location)
+	  System.out.println(bertyDaTurtle);
+	  System.out.println(berthaDaOtherTurtle);
 	  
-	/*
-    // previous examples
-	World earth = new World();
-    Turtle turtle1 = new Turtle(earth);
-    turtle1.drawSquare();
-    
-    World world1 = new World();
-    //Turtle turtle2 = new Turtle(50, 50, world1);
-    Turtle turtle2 = new Turtle(world1);
-    turtle2.setName("Fat Blob");
-    turtle2.drawSquare2();
-    
-    World world2 = new World();
-    Turtle turtle3 = new Turtle(world2);
-    turtle3.setName("Fat Slob");
-    System.out.println(turtle3.getName());
-    turtle3.drawSquarePram(200);
-    
-    System.out.println(world1.getClass());
-    System.out.println(turtle1.getClass());
-    System.out.println(turtle2.getClass());
-    */
+	  // (DebugCode: re-adjust location)
+	  bertyDaTurtle.turn(-90);
+	  berthaDaOtherTurtle.moveTo(220, 55);
+	  berthaDaOtherTurtle.turn(-331);
+	  
+	  // (DebugCode: check location)
+	  System.out.println(bertyDaTurtle);
+	  System.out.println(berthaDaOtherTurtle);
+	  
+	  // Bliss: synchronized swimming
+	  bertyDaTurtle.setPenColor(Color.black);
+	  berthaDaOtherTurtle.setPenColor(Color.orange);
+	  bertyDaTurtle.drawParameterizedRectangle(300,440);
+	  berthaDaOtherTurtle.drawParameterizedRectangle(300,440);
+	  
+
   }
 
 } // this } is the end of class Turtle, put all new methods before this
