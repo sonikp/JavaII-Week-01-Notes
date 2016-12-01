db0f66ee99d4a6ad2f3802fef9a1662202ff62df
Michael.Floerchinger <michael.floerchinger@am.sony.com>
2016-11-30 18:22:28 -0800

    finished MAC

---
+++ src/Chap1Problems.java
@@ -0,0 +1,11 @@
+public class Chap1Problems
+{
+	public static void main(String[] args)
+	{
+		/**
+		 * 1.1 What is a program?
+		 * 
+		 * 
+		 */
+	}
+}
---
+++ src/Chap2Problems.java
@@ -0,0 +1,69 @@
+public class Chap2Problems
+{
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args)
+	{
+		
+		/**
+		 * 2.8 
+		 */
+		
+		double distanceTraveled = 770;
+		double averageSpeed = 60;
+		double time = distanceTraveled / averageSpeed;
+		System.out.println("Time taken (hrs): " + time);
+		
+		distanceTraveled = 770;
+		averageSpeed = 70;
+		time = distanceTraveled / averageSpeed;
+		System.out.println("New time taken (hrs): " + time);
+		
+		
+		/**
+		 * 2.2 - 2.7 What objects would you encounter in a bank?
+		 * teller, manager, accountant, money, checks, credit, debt, overdraft, loan, 
+		 */
+		
+		/**
+		 * 2.1 Computer science concepts
+		 * 
+		 * What is an object?		  
+		 * Objects are persons, places, things that do an action in a situation or are acted upon
+		 * 
+		 * What is a class?		 
+		 * A class in Java tells the computer what data we expect objects of that class to have, and what they can do
+		 * 
+		 * What is a type? Why are types important?
+		 * A type is a description of the "kind of" thing something is. A data type specifies the size and type of value 
+		 * that can be stored in an identifier. It also determines how many bits are used to represent the value and how 
+		 * the bits are interpreted by the computer.
+		 * - Primitive: integer, char, boolean, floating point,
+		 * - Non-primitive: classes, interfaces, arrays
+		 * 
+		 * What is casting? What is it used for?
+		 * Casting is taking an existing data type and transforming it to another data type. It tells 
+		 * the compiler to change a value to a particular type even if it could lead to a loss of data.
+		 * 
+		 * What is a variable? When do you need one? What are the differences between object variables and primitive variables?
+		 * A variable is a data element that is a data type and a data size which is physically located in memory of the computer.
+		 * A variable is used to represent data. Primitive variables are variables that are declared of any of the primitive 
+		 * data types, integer, char boolean, floatingPoint. Object variables are any other type. 
+		 * 
+		 * What is garbage collection?
+		 * Garbage collection is the process where the JVM keeps track of the space being used by the application, and once space 
+		 * is no longer used, it is returned to the system.
+		 * 
+		 * What are relational operators?
+		 * Operators that allow the comparison of one data element or expression to another. greater/less than == / !=
+		 * Maths operator is a + - / *
+		 * 
+		 * What is a string?
+		 * A string is a sequence of characters. In java a string is an object.
+		 * 
+		 */
+	}
+}
